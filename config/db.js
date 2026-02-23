ea936b5d458530fc8fdc8b0125ff36980a1a234d
AldyyNgere <aldyngere20@gmail.com>
2026-02-24 00:27:08 +0800

    fix deploy 2

--- config/db.js
+++ config/db.js
@@ -16,11 +16,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  uri: process.env.MYSQL_URL,
 });
 
 module.exports = connection.promise();
