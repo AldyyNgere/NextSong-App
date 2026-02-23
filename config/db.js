71d27bf679a82ae619cbdcc3b9beffd1318efc3c
AldyyNgere <aldyngere20@gmail.com>
2026-02-24 00:38:55 +0800

    fd4

--- config/db.js
+++ config/db.js
@@ -15,6 +15,12 @@
 
 const mysql = require("mysql2");
 
-const connection = mysql.createPool(process.env.MYSQL_URL);
+const connection = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
 
 module.exports = connection.promise();
