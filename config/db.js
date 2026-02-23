184babadc413ef495b40611f449dd2f5817417eb
AldyyNgere <aldyngere20@gmail.com>
2026-02-24 00:01:00 +0800

    fix deploy

--- config/db.js
+++ config/db.js
@@ -1,14 +1,26 @@
-const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nextsong",
-  port: 3307,
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from(""),
-  },
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "nextsong",
+//   port: 3307,
+//   authPlugins: {
+//     mysql_clear_password: () => () => Buffer.from(""),
+//   },
+// });
+
+// module.exports = pool;
+
+const mysql = require("mysql2");
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool;
+module.exports = connection.promise();
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
