553d71f6d3f889d0af1481e95b48e781220a7001
AldyyNgere <aldyngere20@gmail.com>
2026-02-24 19:35:45 +0800

    fd6

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+// const express = require("express");
+// const cors = require("cors");
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static("public"));
+
+// const songRoutes = require("./routes/songs");
+// app.use("/api/songs", songRoutes);
+
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
+
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +31,9 @@ app.use(express.static("public"));
 const songRoutes = require("./routes/songs");
 app.use("/api/songs", songRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// WAJIB untuk Railway
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
