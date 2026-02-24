7de2ecf391c61fb7ea015d2b94bba8fd17e9e3c3
AldyyNgere <aldyngere20@gmail.com>
2026-02-24 19:49:33 +0800

    Use MYSQL_URL with SSL

--- config/db.js
+++ config/db.js
@@ -16,11 +16,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  uri: process.env.MYSQL_URL,
   ssl: {
     rejectUnauthorized: false,
   },
